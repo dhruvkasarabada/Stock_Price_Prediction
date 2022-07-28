02c1f765feadaa1dc373340bd7b92bf79cf549af
Dhruv Kasarabada <101069265+dhruvkasarabada@users.noreply.github.com>
2022-07-28 15:46:55 -0700

    Update modules.py

--- AIModel/modules.py
+++ AIModel/modules.py
@@ -1,1 +1,147 @@
+#libraries
 
+import numpy as np
+import pandas as pd
+import math
+import sklearn
+import sklearn.preprocessing
+import datetime
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+#access data
+df = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/Columbia Project/prices-split-adjusted.csv.zip", index_col = 0)
+df.info()
+df.head()
+
+#load data
+def load_data(stock, seq_len):
+    data_raw = stock.values # convert to numpy array
+    data = []
+    
+    # create all possible sequences of length seq_len
+    for index in range(len(data_raw) - seq_len): 
+        data.append(data_raw[index: index + seq_len])
+    
+    data = np.array(data);
+    valid_set_size = int(np.round(valid_set_size_percentage/100*data.shape[0]));  
+    test_set_size = int(np.round(test_set_size_percentage/100*data.shape[0]));
+    train_set_size = data.shape[0] - (valid_set_size + test_set_size);
+    
+    x_train = data[:train_set_size,:-1,:]
+    y_train = data[:train_set_size,-1,:]
+    
+    x_valid = data[train_set_size:train_set_size+valid_set_size,:-1,:]
+    y_valid = data[train_set_size:train_set_size+valid_set_size,-1,:]
+
+    x_test = data[train_set_size+valid_set_size:,:-1,:]
+    y_test = data[train_set_size+valid_set_size:,-1,:]
+    
+    return [x_train, y_train, x_valid, y_valid, x_test, y_test]
+  
+#modeling data
+index_in_epoch = 0;
+perm_array  = np.arange(x_train.shape[0])
+np.random.shuffle(perm_array)
+
+# function to get the next batch
+def get_next_batch(batch_size):
+    global index_in_epoch, x_train, perm_array   
+    start = index_in_epoch
+    index_in_epoch += batch_size
+    
+    if index_in_epoch > x_train.shape[0]:
+        np.random.shuffle(perm_array) # shuffle permutation array
+        start = 0 # start next epoch
+        index_in_epoch = batch_size
+        
+    end = index_in_epoch
+    return x_train[perm_array[start:end]], y_train[perm_array[start:end]]
+
+# parameters
+n_steps = seq_len-1 
+n_inputs = 4 
+n_neurons = 200 
+n_outputs = 4
+n_layers = 2
+learning_rate = 0.001
+batch_size = 50
+n_epochs = 100 
+train_set_size = x_train.shape[0]
+test_set_size = x_test.shape[0]
+
+tf.compat.v1.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_outputs])
+
+# use Basic RNN Cell
+layers = [tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, activation=tf.nn.elu)
+          for layer in range(n_layers)]
+# run graph
+with tf.Session() as sess: 
+    sess.run(tf.global_variables_initializer())
+    for iteration in range(int(n_epochs*train_set_size/batch_size)):
+        x_batch, y_batch = get_next_batch(batch_size) # fetch the next training batch 
+        sess.run(training_op, feed_dict={X: x_batch, y: y_batch}) 
+        if iteration % int(5*train_set_size/batch_size) == 0:
+            mse_train = loss.eval(feed_dict={X: x_train, y: y_train}) 
+            mse_valid = loss.eval(feed_dict={X: x_valid, y: y_valid}) 
+            print('%.2f epochs: MSE train/valid = %.6f/%.6f'%(
+                iteration*batch_size/train_set_size, mse_train, mse_valid))
+
+    y_train_pred = sess.run(outputs, feed_dict={X: x_train})
+    y_valid_pred = sess.run(outputs, feed_dict={X: x_valid})
+    y_test_pred = sess.run(outputs, feed_dict={X: x_test})
+ft = 0 # 0 = open, 1 = close, 2 = highest, 3 = lowest
+
+## show predictions
+plt.figure(figsize=(15, 5));
+plt.subplot(1,2,1);
+
+plt.plot(np.arange(y_train.shape[0]), y_train[:,ft], color='blue', label='train target')
+
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_valid.shape[0]), y_valid[:,ft],
+         color='gray', label='valid target')
+
+plt.plot(np.arange(y_train.shape[0]+y_valid.shape[0],
+                   y_train.shape[0]+y_test.shape[0]+y_test.shape[0]),
+         y_test[:,ft], color='black', label='test target')
+
+plt.plot(np.arange(y_train_pred.shape[0]),y_train_pred[:,ft], color='red',
+         label='train prediction')
+
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_valid_pred.shape[0]),
+         y_valid_pred[:,ft], color='orange', label='valid prediction')
+
+plt.plot(np.arange(y_train_pred.shape[0]+y_valid_pred.shape[0],
+                   y_train_pred.shape[0]+y_valid_pred.shape[0]+y_test_pred.shape[0]),
+         y_test_pred[:,ft], color='green', label='test prediction')
+
+plt.title('past and future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best');
+plt.subplot(1,2,2);
+
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]),
+         y_test[:,ft], color='black', label='test target')
+
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_test_pred.shape[0]),
+         y_test_pred[:,ft], color='green', label='test prediction')
+
+plt.title('future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best');
+
+corr_price_development_train = np.sum(np.equal(np.sign(y_train[:,1]-y_train[:,0]),
+            np.sign(y_train_pred[:,1]-y_train_pred[:,0])).astype(int)) / y_train.shape[0]
+corr_price_development_valid = np.sum(np.equal(np.sign(y_valid[:,1]-y_valid[:,0]),
+            np.sign(y_valid_pred[:,1]-y_valid_pred[:,0])).astype(int)) / y_valid.shape[0]
+corr_price_development_test = np.sum(np.equal(np.sign(y_test[:,1]-y_test[:,0]),
+            np.sign(y_test_pred[:,1]-y_test_pred[:,0])).astype(int)) / y_test.shape[0]
+
+print('correct sign prediction for close - open price for train/valid/test: %.2f/%.2f/%.2f'%(
+    corr_price_development_train, corr_price_development_valid, corr_price_development_test))
