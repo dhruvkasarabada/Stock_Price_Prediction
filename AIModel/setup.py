fa7220a698f4b1b1b73e69495a31b3f5f27f012e
Dhruv Kasarabada <101069265+dhruvkasarabada@users.noreply.github.com>
2022-07-28 15:55:45 -0700

    Create setup.py

---
+++ AIModel/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="AICourse",
+  author="Dhruv Kasarabada",
+  author_email="dhruv.kasarabada@gmail.com",
+  license="MIT license",
+  description="This package provides the AI model for prediction of Stock Prices in NY.",
+  packages=["AIModel"]
+)
